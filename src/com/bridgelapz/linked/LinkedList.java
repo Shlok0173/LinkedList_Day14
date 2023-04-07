6e4ec483486a617ee792ac03eb58eab3b6ece8ed
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-04-07 08:52:32 +0530

    Delete given Postion

--- src/com/bridgelapz/linked/LinkedList.java
+++ src/com/bridgelapz/linked/LinkedList.java
@@ -2,63 +2,74 @@ package com.bridgelapz.linked;
 
 public class LinkedList {
 
-	   public Node head;
-	    public Node tail;
+    public Node head;
+    public Node tail;
 
-	    static class Node {
-	        int data;
-	        Node next;
+    static class Node {
+        int data;
+        Node next;
 
-	        Node(int d) {
-	            data = d;
-	            next = null;
-	        }
-	    }
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
 
-	    // Adding new Node with values
-	    public void add(int datas) {
-	        Node newNode = new Node(datas);
-	        if (head == null) {
-	            head = newNode;
-	            tail = newNode;
-	        } else {
-	            newNode.next = head;
-	            head = newNode;
-	        }
-	    }
+    // Adding new Node with values
+    public void add(int datas) {
+        Node newNode = new Node(datas);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
 
-	    // Display the Node with value
-	    public void display() {
-	        Node current = head;
-	        if (head == null) {
-	            System.out.println("List is empty");
-	        }
-	        while (current != null) {
-	            System.out.println(current.data + " ");
-	            current = current.next;
-	        }
-	    }
+    // Display the Node with value
+    public void display() {
+        Node current = head;
+        if (head == null) {
+            System.out.println("List is empty");
+        }
+        while (current != null) {
+            System.out.println(current.data + " ");
+            current = current.next;
+        }
+    }
 
-	    // Deleting the FirstNode with Value
-	    public void pop() {
-	        Node current = head;
-	        if (current == head) {
-	            head = head.next;
-	            current = head;
-	        }
-	    }
+    // Deleting the LastNode with Value
+    public void pop() {
 
-	    public static void main(String[] args) {
-	        LinkedList list = new LinkedList();
+        //Checks if the list is empty
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        } else {
+            if (head != tail) {
+                Node current = head;
+                while (current.next != tail) {
+                    current = current.next;
+                }
+                tail = current;
+                tail.next = null;
+            } else {
+                head = tail = null;
+            }
+        }
+    }
 
-	        list.add(70);
-	        list.add(30);
-	        list.add(56);
-	        list.display();
-	        list.pop();
-	        list.display();
-	    }
-	}
+    public static void main(String[] args) {
+       LinkedList list = new LinkedList();
+
+        list.add(70);
+        list.add(30);
+        list.add(56);
+        list.display();
+        list.pop();
+        list.display();
+    }
 
 	}
 	
